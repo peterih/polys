e8f97ad6d31fe46ec0e9ad4b2f1eb954d66e91f1
Peter I. Hansen <piha@life.ku.dk>
2010-11-02 14:45:09 +0100

    Fix coding style.

--- generate.c
+++ generate.c
@@ -1,33 +1,27 @@
-/**********************************************************************
-POLYS MODULE <generate.c>
-
-   Written by Soren Balling Engelsen, INRA-93.
-**********************************************************************/
+/*
+ * Written by Soren Balling Engelsen, INRA-93.
+ */
 #include "polys.h"
 #include "extern.h"
 
-
-/**********************************************************************
-   This rutine will generate a table of bonds B[] and check if the 
-valencies are saturated.
-   Using this table it will generate a table of valence angles (theta) 
-T[] and a table of torsional angles (phi) P[] for the polysaccharide
-**********************************************************************/
-void Generate(int option)
-
-{  double tstart;
+/*
+ * This rutine will generate a table of bonds B[] and check if the 
+ * valencies are saturated.
+ * Using this table it will generate a table of valence angles (theta) 
+ * T[] and a table of torsional angles (phi) P[] for the polysaccharide
+ */
+void
+Generate(int option)
+{
+   double tstart;
 
    tstart = cput();
    if (M.nbond == 0)
-     gen_bondtab();
+      gen_bondtab();
    gen_angletab();
    gen_tortab();
    gen_nonbond();
    if (option >= 10)
       gen_rotvec();
    tint += cput() - tstart;
-
-}  /* End of Generate */
-
-
-/* End of file */
+}
