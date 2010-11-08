9ae080d5f7598adcdc79295fcc8cd6b93fedda06
Peter I. Hansen <piha@life.ku.dk>
2010-11-08 11:50:31 +0100

    Recompiled the parser, and updated install info.

--- polypars.tab.h
+++ polypars.tab.h
@@ -1,110 +1,163 @@
-#ifndef BISON_POLYPARS_TAB_H
-# define BISON_POLYPARS_TAB_H
 
-#ifndef YYSTYPE
-typedef union { double   real;
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     REAL = 258,
+     INTEGER = 259,
+     QSTRING = 260,
+     NAME = 261,
+     NL = 262,
+     ATTACH = 263,
+     BUILD = 264,
+     CALC = 265,
+     COORD = 266,
+     CREATE = 267,
+     DATE = 268,
+     DOUBLEHELIX = 269,
+     END = 270,
+     ENER = 271,
+     GENERATE = 272,
+     INIT = 273,
+     HELIX = 274,
+     HELP = 275,
+     LABEL = 276,
+     MINI = 277,
+     PRIMARY = 278,
+     READ = 279,
+     RECORD = 280,
+     MODIFY = 281,
+     SET = 282,
+     TIME = 283,
+     WRITE = 284,
+     STOP = 285,
+     NSETS = 286,
+     SAVE = 287,
+     DIST = 288,
+     DTAB = 289,
+     MASS = 290,
+     VOLUME = 291,
+     RGYR = 292,
+     COPY = 293,
+     ORIENT = 294,
+     RMS = 295,
+     STAT = 296,
+     DERIV = 297,
+     NFOLD = 298,
+     OPTIMIZE = 299,
+     NREPS = 300,
+     NSTR = 301,
+     SHIFT = 302,
+     ONE = 303,
+     ALL = 304,
+     BOND = 305,
+     NBOND = 306,
+     ANGLE = 307,
+     TORSION = 308,
+     ROTBOND = 309,
+     CARTE = 310,
+     DIHED = 311,
+     FRAC = 312,
+     GRDTOL = 313,
+     ENETOL = 314,
+     MAXIT = 315,
+     MAXFUN = 316,
+     SILENT = 317,
+     NOISE = 318,
+     SYMMETRY = 319,
+     INTERNAL = 320,
+     PARAM = 321,
+     CHARGES = 322,
+     REF = 323,
+     PHI = 324,
+     PHIB = 325,
+     PHIE = 326,
+     PHIINC = 327,
+     PSI = 328,
+     PSIB = 329,
+     PSIE = 330,
+     PSIINC = 331,
+     RECOPT = 332,
+     DEBUG = 333,
+     ID = 334,
+     TITLE = 335,
+     RANSEED = 336,
+     AMBER = 337,
+     CHARMM = 338,
+     CFF = 339,
+     CSD = 340,
+     DAT = 341,
+     MM3 = 342,
+     MONO = 343,
+     PDB = 344,
+     PF = 345,
+     PLOT = 346,
+     SYBYL = 347,
+     SPF = 348,
+     PIM = 349,
+     XYZ = 350
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 82 "polypars"
+ double   real;
          int      integer;
          char     *string;
          int      cmd;
-       } yystype;
-# define YYSTYPE yystype
-# define YYSTYPE_IS_TRIVIAL 1
-#endif
-# define	REAL	257
-# define	INTEGER	258
-# define	QSTRING	259
-# define	NAME	260
-# define	NL	261
-# define	ATTACH	262
-# define	BUILD	263
-# define	CALC	264
-# define	COORD	265
-# define	CREATE	266
-# define	DATE	267
-# define	DOUBLEHELIX	268
-# define	END	269
-# define	ENER	270
-# define	GENERATE	271
-# define	INIT	272
-# define	HELIX	273
-# define	HELP	274
-# define	LABEL	275
-# define	MINI	276
-# define	PRIMARY	277
-# define	READ	278
-# define	RECORD	279
-# define	MODIFY	280
-# define	SET	281
-# define	TIME	282
-# define	WRITE	283
-# define	STOP	284
-# define	NSETS	285
-# define	SAVE	286
-# define	DIST	287
-# define	DTAB	288
-# define	MASS	289
-# define	VOLUME	290
-# define	RGYR	291
-# define	COPY	292
-# define	ORIENT	293
-# define	RMS	294
-# define	STAT	295
-# define	DERIV	296
-# define	NFOLD	297
-# define	OPTIMIZE	298
-# define	NREPS	299
-# define	NSTR	300
-# define	SHIFT	301
-# define	ONE	302
-# define	ALL	303
-# define	BOND	304
-# define	NBOND	305
-# define	ANGLE	306
-# define	TORSION	307
-# define	ROTBOND	308
-# define	CARTE	309
-# define	DIHED	310
-# define	FRAC	311
-# define	GRDTOL	312
-# define	ENETOL	313
-# define	MAXIT	314
-# define	MAXFUN	315
-# define	SILENT	316
-# define	NOISE	317
-# define	SYMMETRY	318
-# define	INTERNAL	319
-# define	PARAM	320
-# define	CHARGES	321
-# define	REF	322
-# define	PHI	323
-# define	PHIB	324
-# define	PHIE	325
-# define	PHIINC	326
-# define	PSI	327
-# define	PSIB	328
-# define	PSIE	329
-# define	PSIINC	330
-# define	RECOPT	331
-# define	DEBUG	332
-# define	ID	333
-# define	TITLE	334
-# define	RANSEED	335
-# define	AMBER	336
-# define	CHARMM	337
-# define	CFF	338
-# define	CSD	339
-# define	DAT	340
-# define	MM3	341
-# define	MONO	342
-# define	PDB	343
-# define	PF	344
-# define	PLOT	345
-# define	SYBYL	346
-# define	SPF	347
-# define	PIM	348
-# define	XYZ	349
+       
 
+
+/* Line 1676 of yacc.c  */
+#line 155 "polypars.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
 
 extern YYSTYPE yylval;
 
-#endif /* not BISON_POLYPARS_TAB_H */
+
